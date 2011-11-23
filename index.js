4cd8a95a7a170689148b601a6d246ac50205f871
Biagio Arobba <barobba@gmail.com>
2011-11-23 00:02:06 -0700

    Major updates.
    Signed-off-by: Biagio Arobba <barobba@gmail.com>

---
+++ index.js
@@ -0,0 +1,68 @@
+
+
+var firstPack = null;
+
+
+$(window).ready(function(){
+
+
+  // Find out what packages are available
+  $.getJSON('packs_cards/_files.json', function(files, textStatus){
+
+    for (fileIndex in files) {
+
+      // Assign the first pack
+      if(!firstPack) {
+        firstPack = fileIndex;
+        $('iframe').attr('src', 'index_game.html?pack='+firstPack);
+      }
+      
+      // Cycling through packages, to create menu of "packs" to choose from
+      if(typeof files[fileIndex] === 'object') {
+        
+        var filepath = 'packs_cards/' + fileIndex + '/_data.json';
+        $.ajax({
+          url: filepath,
+          dataType: 'json',
+          async: false,
+          success: function(cardData){
+            var cards = cardData.cards;
+            for (cardIndex in cards) {
+              
+              var card = cards[cardIndex];
+              card.packID = fileIndex;
+              var listItem = themePackListItem(card);
+              listItem.appendTo('.pack-list-container');
+              
+              break;
+              // We are using first card as "title" for now.
+              // So, only loop though the first time, then, break.
+            }
+            
+          }
+        })
+        .error(function(){
+          alert('Could not get _data.json file');
+        });
+        
+      }
+      
+    }
+    
+  })
+  .error(function(){
+    alert('There was an error getting the file.');
+  });
+
+  
+  
+  
+});
+
+
+function themePackListItem(pack) {
+  var listItem = $("<div class='game-list-item'><div class='game-list-item-title'>TITLE</div></div>")
+  var link = $('<a />').attr('href', "index_game.html?pack="+pack.packID).attr('target', 'cardgame').html(pack.text.example.text);
+  listItem.children('.game-list-item-title').html(link);
+  return listItem;
+}
--- js/script.js
+++ js/script.js
@@ -3,91 +3,183 @@
 */
 
 $(document).ready(function () {
-	var deck = "packs_cards/27617/";
+  
+  var packID = getParameterByName('pack');
+	var deck = "packs_cards/"+packID+"/";
 
 	$.getJSON(deck + "_data.json",
 		function(cards) {
-			var img_src, aud_src, id;
-			var allcards = [];
 
+      $('.ajax-loader-icon').remove();
+	  
+	    cards = cards.cards;
+			var img_src, aud_src, id;
+
+			// Create the DECK
+			var allcards = [];
+      // Add all the cards
 			for(card in cards) {
 				allcards.push(cards[card]);
-				allcards.push(cards[card]);
 			}
-
+			// Shuffle the deck
 			allcards = $.shuffle(allcards);
+      // Pick just the top 10 cards
+      allcards = allcards.slice(0, 10);
+      // Duplicate the cards in the deck
+      allcards = allcards.concat(allcards);
 
+			// Layout the cards on the game board 
 			$.each(allcards, function(key, val) {
-				img_src = val.pictureURL;
+			  
+				//img_src = val.pictureURL;
+			  img_src = 'packs_cards/' + packID + '/' + val.pictureFilename;
+				
 				id = val.text.example.audioID;
 				aud_src = val.text.example.audioID;
 				word = val.text.example.text;
 
-				$('<dt class="card back" data-word="'+ word +'" id="'+id+'"><img src="img/back_of_card.png" alt="" /></dt>').click(function() { flipCard($(this)); }).appendTo("#game");
-				$('<dd class="card front" id="'+id+'_flip"><img src="' + img_src +'" alt="" /><audio id="' + id + '_audio" preload="auto"><source src="' + deck + aud_src + '.ogg" type="audio/ogg" /></audio></dt>').appendTo("#game");
+				// Back of card
+				$('<dt class="card back" data-word="'+ word +'" id="'+id+'"><img src="img/back_of_card.png" alt="" /></dt>')
+				.click(function() {
+				  
+				  // ALL THE CARDS START WITH THE CLICKING BEING HANDLED BY THE "FLIP CARD" FUNCTION  
+				  clickProxy(this, function(card){
+	          flipCard(card); 
+				  });
+				  
+				}).appendTo("#game");
+				
+				// Front of card
+				$('<dd class="card front" id="'+id+'_flip"><img src="' + img_src +'" alt="" /><audio id="' + id + '_audio" preload="auto"><source src="' + deck + aud_src + '.ogg" type="audio/ogg" /></audio></dt>')
+				.appendTo("#game");
+				
 			});
+			
+			// For some reason, remove the first element...
+			// TODO: Track down where this bug is coming from, so we can get rid of this special case.
+			$('#game').children().first().remove();
+      			
 		});
 });
 
+/**
+ * Click handler decides whether or not to perform click action
+ * @return
+ */
+function clickProxy(selector, method) {
+  var flipped_cards = $(".flipped");
+  if(flipped_cards.length < 2) {
+    method($(selector));
+  }
+}
+
 function flipCard(el) {
+  
 	var new_id = el.attr("id") + "_flip";
 	var aud_id = el.attr("id") + "_audio";
 
+	// This card is considered flipped from the very beginning
+	// Because it has passed through the clickProxy() filter,
+	// Which only allows flipCard to run if there are less than 2 cards flipped.
+  el.addClass("flipped");
+	
+
+  // Start the flip
 	el.flip({
 		direction: "lr",
 		color: "white",
 		content: $('#' + new_id),
+  	
+  	// When the flip is finished, then start playing the audio 
 		onEnd: function() {
-			if(this.bgColor === "transparent") {
-				document.getElementById(aud_id).play();
-				document.getElementById(aud_id).onended = function() {
-					resetAudio(this.id);
-					checkForMatch();
-				};
-			}
+	  
+      if (el.hasClass('flipped')) {
+        document.getElementById(aud_id).play();
+      }
+	  
+	    var flipped_cards = $(".flipped");
+	    if(flipped_cards.length >= 2) {
+	      // Make sure audio will stop when finished playing
+	      // If this is the second flip, then also check for a match
+	      // When the audio is finished playing
+	      document.getElementById(aud_id).addEventListener('ended', function() {
+	        checkForMatch();
+	        checkForWin();
+	      }, false);
+        
+	    }
+	    
+	    // Not sure what 'transparent' represents...we may not need it.
+			//if(this.bgColor === "transparent") {
+			//	document.getElementById(aud_id).play();
+			//}
 		}
 	});
 
-	el.addClass("flipped").unbind("click");
-	el.click(function() { revertCard(el); });
+	// Let the application flip the cards back, based on matches
+	//el.click(function() { revertCard(el); });
+	
 }
 
+
+
+
+
 function resetAudio(audio_id) {
-	document.getElementById(audio_id).pause()
-    document.getElementById(audio_id).currentTime = 0;
+	document.getElementById(audio_id).pause();
+  document.getElementById(audio_id).currentTime = 0;
 }
 
 function checkForMatch() {
+  
 	var flipped_cards = $(".flipped");
+	if($(flipped_cards[0]).data("word") == $(flipped_cards[1]).data("word")) {
+	  
+	  // Mark the cards as matched
+		flipped_cards.each(function(key, val) {
+			$(this).removeClass("flipped").addClass("matched");
+		});
 
-	if(flipped_cards.length === 2) {
-		if($(flipped_cards[0]).data("word") == $(flipped_cards[1]).data("word")) {
-			flipped_cards.each(function(key, val) {
-				$(this).removeClass("flipped").addClass("matched");
-				$(this).unbind("click");
-
-				checkForWin();
-			});
-
-		} else {
-			flipped_cards.each(function(key, val) { $(this).click() });
-		}
+	} else {
+	  
+    // Flip the cards back over
+    flipped_cards.each(function(key, val) { 
+      $(this).removeClass("flipped");
+      $(this).revertFlip({
+        onEnd: function(){}
+      });
+    });
+    
 	}
+	
 }
 
 function checkForWin() {
 	var cards = $("#game dd");
-
 	if(cards.length - 1 === $(".matched").length) {
-		setTimeout(function() {
-			location.reload(true);
-		}, 5000)
+	  alert('Congratulations, you matched all the cards!');
+		location.reload(true);
 	}
 }
 
-function revertCard(el) {
-	el.revertFlip();
-	
-	el.removeClass("flipped").unbind("click");
-	el.click(function() { flipCard(el); });
+
+
+/**
+ * Returns a query string value from the page URL, given the name.
+ * 
+ * @param name
+ * @return
+ */
+function getParameterByName(name)
+{
+  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+  var regexS = "[\\?&]" + name + "=([^&#]*)";
+  var regex = new RegExp(regexS);
+  var results = regex.exec(window.location.href);
+  if(results == null)
+    return "";
+  else
+    return decodeURIComponent(results[1].replace(/\+/g, " "));
 }
+
+
